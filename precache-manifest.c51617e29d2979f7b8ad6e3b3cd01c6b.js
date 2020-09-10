67b96f49f9444294fce811b456e054b8ca061473
unknown <alvinajay95@gmail.com>
2020-09-10 21:54:13 +0530

    Updates

---
+++ precache-manifest.c51617e29d2979f7b8ad6e3b3cd01c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61c0473977d013a8c98375d0094086b",
+    "url": "/mobiotics-shopping-cart/index.html"
+  },
+  {
+    "revision": "cfdf9ddc30c364765894",
+    "url": "/mobiotics-shopping-cart/static/css/main.4631459e.chunk.css"
+  },
+  {
+    "revision": "8fd1d1f67be8aec0ca43",
+    "url": "/mobiotics-shopping-cart/static/js/2.03716101.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mobiotics-shopping-cart/static/js/2.03716101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfdf9ddc30c364765894",
+    "url": "/mobiotics-shopping-cart/static/js/main.91f8b7c2.chunk.js"
+  },
+  {
+    "revision": "5a5be2c8b0975748a2ff",
+    "url": "/mobiotics-shopping-cart/static/js/runtime-main.4f0f696d.js"
+  }
+]);
